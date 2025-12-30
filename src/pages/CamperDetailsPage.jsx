fd40c4447b7cbac2ce9d5f77f61211e614c211c0
bilisimfatihi <bilisimfatihi@gmail.com>
2025-12-31 00:58:15 +0300

    feat add camper details page basic

--- src/pages/CamperDetailsPage.jsx
+++ src/pages/CamperDetailsPage.jsx
@@ -1,7 +1,42 @@
-import React from "react";
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { NavLink, Outlet, useParams } from "react-router-dom";
+import { getCamperById } from "../redux/campers/campersOps";
+import Loader from "../components/Loader/Loader";
 
 const CamperDetailsPage = () => {
-  return <div>CamperDetailsPage</div>;
+  const { id } = useParams();
+  const dispatch = useDispatch();
+  const { selectedCamper, loading, error } = useSelector(
+    (state) => state.campers
+  );
+
+  useEffect(() => {
+    dispatch(getCamperById(id));
+  }, [dispatch, id]);
+
+  if (loading || !selectedCamper) {
+    return <Loader />;
+  }
+
+  if (error) {
+    return <p>Error: {error}</p>;
+  }
+
+  return (
+    <div>
+      <h2>{selectedCamper.name}</h2>
+      <p>Location: {selectedCamper.location}</p>
+      <p>Price: ${selectedCamper.price}</p>
+
+      <nav>
+        <NavLink to="features">Features</NavLink>
+        <NavLink to="reviews">Reviews</NavLink>
+      </nav>
+
+      <Outlet />
+    </div>
+  );
 };
 
 export default CamperDetailsPage;
--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -2,7 +2,9 @@ import { Routes, Route } from "react-router-dom";
 import Layout from "../components/Layout/Layout.jsx";
 import HomePage from "../pages/HomePage.jsx";
 import CatalogPage from "../pages/CatalogPage.jsx";
-import CamperDetailsPage from "../pages/CamperDetailsPAge.jsx";
+import CamperDetailsPage from "../pages/CamperDetailsPage.jsx";
+import Features from "../components/Features/Features.jsx";
+import Reviews from "../components/Reviews/Reviews.jsx";
 
 const AppRouter = () => {
   return (
@@ -10,7 +12,10 @@ const AppRouter = () => {
       <Route path="/" element={<Layout />}>
         <Route index element={<HomePage />} />
         <Route path="/catalog" element={<CatalogPage />} />
-        <Route path="/catalog/:id" element={<CamperDetailsPage />} />
+        <Route path="/catalog/:id" element={<CamperDetailsPage />}>
+          <Route path="features" element={<Features />} />
+          <Route path="reviews" element={<Reviews />} />
+        </Route>
       </Route>
     </Routes>
   );
